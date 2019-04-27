06b310f1ce8935995412443ca41cb1585d150b85
Jeremy Gill <jgill@parallax-innovations.com>
2019-04-27 12:51:06 -0400

    ENH: begin work on test infrastructure

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,3 +1,24 @@
-def test_dummy():
-  """dummy test"""
-  pass
+def test_imports():
+  '''test that major imports work'''
+  from vtkAtamai import (ActorFactory, ImagePane, ImagePlaneFactory, OrthoPlanesFactory,
+                        PaneFrame, RenderPane, RenderPane2D, WindowLevelLabel)
+
+def callback(evt, values):
+  print("vtkAtamai Modified event callback called")
+
+def test_actor_factory():
+  from vtkAtamai import ActorFactory
+
+  o = ActorFactory.ActorFactory()
+  o.AddObserver("ModifiedEvent", callback)
+  o.InvokeEvent('ModifiedEvent')
+  o.tearDown()
+
+def test_image_pane():
+  from vtkAtamai import ImagePane
+  pane = ImagePane.ImagePane()
+
+def test_plane_outline_factory():
+  ''' is no longer used it seems'''
+  from vtkAtamai import PlaneOutlineFactory
+  o = PlaneOutlineFactory.PlaneOutlineFactory()
