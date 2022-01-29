afcf8faa2ca21ee14e86012229415b3ef3e1b34f
Jeremy Gill <jgill@parallax-innovations.com>
2022-01-29 14:09:08 -0500

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requires = [
 ]
 
 setup(name="vtkAtamai",
-      version="2.5.0",
+      version="2.6.0",
       description="Atamai VTK/Python visualization classes",
       long_description="""
   Core VTK/Python classes used by MicroView.
